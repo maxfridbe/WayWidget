a37c0b94e6418a805e41c30d36312aa55d935ced
Max Fridbe <maxfridbe@example.com>
2026-03-05 12:43:34 -0500

    chore: Bump version to 0.1.3

--- package.sh
+++ package.sh
@@ -23,7 +23,7 @@ cp waywidget/target/release/waywidget dest/bin/
 echo "Building RPM..."
 mkdir -p ~/rpmbuild/{SOURCES,SPECS,RPMS,SRPMS,BUILD,BUILDROOT}
 # We create a minimal tarball just for the spec's sake
-tar -czf ~/rpmbuild/SOURCES/waywidget-0.1.2.tar.gz --exclude='./waywidget/target' --transform 's,^,waywidget-0.1.2/,' .
+tar -czf ~/rpmbuild/SOURCES/waywidget-0.1.3.tar.gz --exclude='./waywidget/target' --transform 's,^,waywidget-0.1.3/,' .
 cp packaging/waywidget.spec ~/rpmbuild/SPECS/
 rpmbuild -ba ~/rpmbuild/SPECS/waywidget.spec
 cp ~/rpmbuild/RPMS/x86_64/*.rpm dest/
--- packaging/waywidget.spec
+++ packaging/waywidget.spec
@@ -1,5 +1,5 @@
 Name:           waywidget
-Version:        0.1.2
+Version:        0.1.3
 Release:        1%{?dist}
 Summary:        SVG-to-Cairo Wayland Widget System
 
