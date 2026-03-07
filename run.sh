c8918b044c5ee872c0c1ab860c110c5476968d3a
Max Fridbe <maxfridbe@example.com>
2026-03-07 16:29:03 -0500

    feat: add --desktop support to run.sh helper script

--- run.sh
+++ run.sh
@@ -10,6 +10,12 @@ cd ..
 
 BINARY="./waywidget/target/debug/waywidget"
 
+# Check for desktop flag as second argument
+DESKTOP_ARG=""
+if [ "$2" == "--desktop" ]; then
+    DESKTOP_ARG="--desktop"
+fi
+
 # Function to run example
 run_example() {
     local name=$1
@@ -18,7 +24,7 @@ run_example() {
     local w=$4
     local h=$5
     echo "Running example: $name"
-    local cmd="$BINARY --svg $svg --width $w --height $h"
+    local cmd="$BINARY --svg $svg --width $w --height $h $DESKTOP_ARG"
     if [ -n "$script" ]; then
         cmd="$cmd --script $script"
     fi
@@ -32,25 +38,25 @@ case $EXAMPLE in
     "all")
         echo "Launching all widgets..."
         # Left side
-        $BINARY --svg examples/lcars_clock/widget.svg --script examples/lcars_clock/widget.js --width 600 --height 300 --position 50,50 &
+        $BINARY --svg examples/lcars_clock/widget.svg --script examples/lcars_clock/widget.js --width 600 --height 300 --position 50,50 $DESKTOP_ARG &
         PID1=$!
-        $BINARY --svg examples/ip_visualizer/widget.svg --script examples/ip_visualizer/widget.js --width 350 --height 200 --position 50,400 &
+        $BINARY --svg examples/ip_visualizer/widget.svg --script examples/ip_visualizer/widget.js --width 350 --height 200 --position 50,400 $DESKTOP_ARG &
         PID2=$!
         
         # Middle
-        $BINARY --svg examples/sunrise/widget.svg --script examples/sunrise/widget.js --width 800 --height 450 --position 700,50 &
+        $BINARY --svg examples/sunrise/widget.svg --script examples/sunrise/widget.js --width 800 --height 450 --position 700,50 $DESKTOP_ARG &
         PID3=$!
-        $BINARY --svg examples/clock/clock.svg --script examples/clock/widget.js --width 200 --height 200 --position 700,550 &
+        $BINARY --svg examples/clock/clock.svg --script examples/clock/widget.js --width 200 --height 200 --position 700,550 $DESKTOP_ARG &
         PID4=$!
 
         # Right side
-        $BINARY --svg examples/keyboard/widget.svg --script examples/keyboard/widget.js --width 820 --height 350 --position 1550,50 &
+        $BINARY --svg examples/keyboard/widget.svg --script examples/keyboard/widget.js --width 820 --height 350 --position 1550,50 $DESKTOP_ARG &
         PID5=$!
-        $BINARY --svg examples/warpcore/widget.svg --script examples/warpcore/widget.js --width 150 --height 400 --position 1550,450 &
+        $BINARY --svg examples/warpcore/widget.svg --script examples/warpcore/widget.js --width 150 --height 400 --position 1550,450 $DESKTOP_ARG &
         PID6=$!
 
         # Weather
-        $BINARY --svg examples/weather/widget.svg --script examples/weather/widget.js --width 700 --height 220 --position 700,800 &
+        $BINARY --svg examples/weather/widget.svg --script examples/weather/widget.js --width 700 --height 220 --position 700,800 $DESKTOP_ARG &
         PID7=$!
 
         trap "kill $PID1 $PID2 $PID3 $PID4 $PID5 $PID6 $PID7; exit" INT TERM
@@ -84,6 +90,6 @@ case $EXAMPLE in
         run_example "Static Lion" "examples/lion/widget.svg" "" 200 200
         ;;
     *)
-        echo "Usage: ./run.sh [all|clock|lcars|sunrise|keyboard|warpcore|ip|lion]"
+        echo "Usage: ./run.sh [all|clock|lcars|sunrise|keyboard|warpcore|ip|lion|calculator] [--desktop]"
         ;;
 esac
